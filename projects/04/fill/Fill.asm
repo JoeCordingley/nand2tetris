b455ed188571b170a49c0d528519bd8279709da5
Joe Cordingley <joewcordingley@gmail.com>
2024-02-24 16:10:16 +0000

    personal computer parts

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -11,4 +11,45 @@
 // 'white' in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-//// Replace this comment with your code.
+(RESET)
+  @SCREEN //screenstart
+  D=A //D=screenstart
+  @writelocation
+  M=D //writelocation=screenstart
+(LOOP)
+  @KBD //A=keyboard
+  D=M //D=M[keyboard]
+  @SETONE //A=SETONE
+  D;JNE //jump to SETONE if M[keyboard] != 0
+  @pixelcolour //A=pixelcolour
+  M=0 //M[pixelcolour] = 0
+  @CHANGESCREEN //A=CHANGESCREEN
+  0;JMP //jump to CHANGESCREEN
+(SETONE)
+  @pixelcolour //A=pixelcolour
+  M=-1 //M[pixelcolour] = 1
+(CHANGESCREEN)
+  @pixelcolour //A=pixelcolour
+  D=M //D=M[pixelcolour]
+  @writelocation //A=writelocation
+  A=M
+  M=D //M[writeLocaltion] = M[pixelcolour]
+  @writelocation
+  D=M+1 //D=writeLocation +1
+  @KBD //A=keyboard
+  D=D-A //D=writeLocation + 1 - keyboard
+  @RESET //A=reset
+  D;JEQ //jump if writelocation + 1 == keyboard
+  @writelocation //A=writelocation
+  M=M+1
+  @LOOP
+  0;JMP
+
+
+
+
+
+
+
+
+
