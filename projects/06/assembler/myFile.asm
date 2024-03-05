576ea3b5d341bd8580ab26055aadbd5d1775a3c7
Joe Cordingley <joewcordingley@gmail.com>
2024-03-05 08:18:59 +0000

    assembler in progress

--- projects/06/assembler/myFile.asm
+++ projects/06/assembler/myFile.asm
@@ -1,1 +1,2 @@
-here
+D=A
+D=x
--- projects/06/assembler/src/Lib.hs
+++ projects/06/assembler/src/Lib.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Lib
     ( printFile
     , InputFile(..)
@@ -5,17 +7,45 @@ module Lib
     , assemble
     ) where
 
+import System.IO (withFile, IOMode(..))
 import Pipes
-import Pipes.Text (Text)
-import Pipes.Prelude (mapFoldable)
-import qualified Pipes.Text.IO as Text
-import Pipes.Safe
+import Pipes.Prelude (mapFoldable, fromHandle, toHandle)
+import Text.Parsec hiding (string)
+import qualified Text.Parsec as Parsec (string)
+import Control.Monad.Except
 
 data InputFile = InputFile String
 data OutputFile = OutputFile String
 
 printFile :: InputFile -> OutputFile -> IO ()
-printFile (InputFile input) (OutputFile output) = runSafeT $ runEffect $ Text.readFile input >-> mapFoldable assemble >-> Text.writeFile output
+printFile (InputFile input) (OutputFile output) = 
+  withFile input ReadMode $ \i -> 
+    withFile output WriteMode $ \o -> 
+      runEffect $ fromHandle i >-> mapFoldable (ExceptT . assemble)  >-> toHandle o
 
-assemble :: Text -> [Text]
-assemble = undefined
+assemble :: String -> Maybe (Either ParseError String)
+assemble = runParserT cinstruction () "hack"
+
+--line :: ParsecT String () Maybe String
+--line = ((spaces *> lift Nothing) <|> instruction) <* comment
+--
+--comment :: Monad m => ParsecT String () m ()
+--comment = void $ Parsec.string "//" *> manyTill anyChar eof
+
+cinstruction :: Monad m => ParsecT String () m String
+cinstruction = do
+  d <- dest <* char '=' <|> return "000"
+  c <- comp
+  j <- char ';' *> jump <|> return "000"
+  return $ "111" <> c <> d <> j
+
+dest :: Monad m => ParsecT String () m String
+dest = 
+  "001" <$ char 'M' <|>
+  "010" <$ char 'D'
+
+comp :: Monad m => ParsecT String () m String
+comp = "110000" <$ char 'A'
+
+jump :: Monad m => ParsecT String () m String
+jump = "001" <$ Parsec.string "JGT"
