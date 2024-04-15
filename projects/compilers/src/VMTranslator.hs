09a479725afe98bcb7d23ed0020a1b32adccff59
Joe Cordingley <joewcordingley@gmail.com>
2024-04-15 22:00:50 +0100

    wip

--- projects/compilers/src/VMTranslator.hs
+++ projects/compilers/src/VMTranslator.hs
@@ -36,58 +36,65 @@ vminstruction = memoryCommand <|> logicalCommand
       where
         op = push <|> pop
           where
-            push = f <$ string "push"
-              where
-                f s i = s Push i <> pushd
-            pop = f <$ string "pop"
-              where
-                f s i = popd <> s Pop i
-        segment = constant
+            push = ($ Push) <$ string "push"
+            pop = ($ Pop) <$ string "pop"
+        segment = constant <|> local <|> argument <|> this <|> that <|> temp <|> pointer <|> static
           where
             constant = f <$ string "constant"
               where
-                f Push i = ["@" <> show i, "D=A"]
-                f Pop _ = []
+                f Push i = ["@" <> show i, "D=A"] <> pushd
+                f Pop _ = ["@SP", "M=M-1"]
+            temp = rSegment 5 <$ string "temp"
+            pointer = rSegment 3 <$ string "pointer"
+            static = f <$ string "static"
+              where
+                f Push i = ["@Xxx." <> show i, "D=M"] <> pushd
+                f Pop i = popd <> ["@Xxx." <> show i, "M=D"]
+            local = segmentCommand "LCL" <$ string "local"
+            argument = segmentCommand "ARG" <$ string "argument"
+            this = segmentCommand "THIS" <$ string "this"
+            that = segmentCommand "THAT" <$ string "that"
+            rSegment j Push i = ["@R" <> show (i + j), "D=M"] <> pushd
+            rSegment j Pop i = popd <> ["@R" <> show (i + j), "M=D"]
+            segmentCommand seg Push i = ["@" <> seg, "D=M", "@" <> show i, "A=D+A", "D=M"] <> pushd
+            segmentCommand seg Pop i = ["@" <> seg, "D=M", "@" <> show i, "D=D+A", "@R13", "M=D"] <> popd <> ["@R13", "A=M", "M=D"]
         index = int
-    --    push = lexeme (string "push") *> lexeme (constant <$ string "constant") <*> index
-    --      where
-    --        index = int
-    --        constant i = ["@" <> show i, "D=A"] <> pushd
     logicalCommand = add <|> eq <|> lt <|> gt <|> sub <|> neg <|> and' <|> or' <|> not'
       where
-        binaryOperator f name = popd <> popm <> f <> pushd <$ string name
-        relationalOperator :: String -> String -> Parser [String]
-        relationalOperator f name =
-            incrementing $ flip binaryOperator name . relationalCommands f
-        relationalCommands :: String -> Int -> [String]
-        relationalCommands f i =
-            [ "D=M-D"
-            , "@" <> eqi
-            , "D;J" <> f
-            , "D=0"
-            , "@" <> endi
-            , "0;JMP"
-            , "(" <> eqi <> ")"
-            , "D=-1"
-            , "(" <> endi <> ")"
-            ]
-          where
-            eqi = "EQ" <> show i
-            endi = "END" <> show i
-
-        add = binaryOperator ["D=D+M"] "add"
-        sub = binaryOperator ["D=M-D"] "sub"
+        add = simpleBinaryOperator "D=D+M" "add"
+        sub = simpleBinaryOperator "D=M-D" "sub"
         eq = relationalOperator "EQ" "eq"
         lt = relationalOperator "LT" "lt"
         gt = relationalOperator "GT" "gt"
-        and' = binaryOperator ["D=D&M"] "and"
-        or' = binaryOperator ["D=D|M"] "or"
+        and' = simpleBinaryOperator "D=D&M" "and"
+        or' = simpleBinaryOperator "D=D|M" "or"
+        neg = unaryOp "D=-M" "neg"
+        not' = unaryOp "D=!M" "not"
+        simpleBinaryOperator f = binaryOperator [f]
+        relationalOperator f name = incrementing g
+          where
+            g i =
+                binaryOperator
+                    [ "D=M-D"
+                    , "@" <> eqi
+                    , "D;J" <> f
+                    , "D=0"
+                    , "@" <> endi
+                    , "0;JMP"
+                    , "(" <> eqi <> ")"
+                    , "D=-1"
+                    , "(" <> endi <> ")"
+                    ]
+                    name
+              where
+                eqi = "EQ" <> show i
+                endi = "END" <> show i
+        unaryOp f name = popm <> [f] <> pushd <$ string name
+        binaryOperator commands name = popd <> popm <> commands <> pushd <$ string name
         popm =
             [ "@SP"
             , "AM=M-1"
             ]
-        neg = popm <> ["D=-M"] <> pushd <$ string "neg"
-        not' = popm <> ["D=!M"] <> pushd <$ string "not"
     pushd = ["@SP", "A=M", "M=D", "@SP", "M=M+1"]
     popd =
         [ "@SP"
